79a58dfb75809263fbaa553acd4f64f6ee72966e
Sadeepa lakshan <as.lakshan03@gmail.com>
2025-09-25 13:55:43 +0530

    BO factory updates

--- src/main/java/lk/ijse/bo/BOFactory.java
+++ src/main/java/lk/ijse/bo/BOFactory.java
@@ -6,16 +6,17 @@ import lk.ijse.bo.custom.impl.*;
 public class BOFactory {
 
     public enum BOType {
-        SIGNUP,LOGIN,STUDENT,COURSE,INSTRUCTOR,LESSON
+        SIGNUP,LOGIN,STUDENT,COURSE,INSTRUCTOR,LESSON,PAYMENT
     }
     public static SuperBO getBO(BOType type) {
          return switch (type) {
-            case SIGNUP -> new SignUpBOImpl();
+             case SIGNUP -> new SignUpBOImpl();
              case LOGIN -> new LoginBOImpl();
              case STUDENT -> new StudentBOImpl();
              case COURSE -> new CourseBOImpl();
              case INSTRUCTOR -> new InstructorBOImpl();
-                case LESSON -> new LessonBOImpl();
+             case LESSON -> new LessonBOImpl();
+             case PAYMENT -> new PaymentBOImpl();
             default -> null;
         };
     }
