3c2e7c079dace41be52ed9fe16ff55db9f77a827
Sadeepa lakshan <as.lakshan03@gmail.com>
2025-09-25 13:56:24 +0530

    Payment DAO

---
+++ src/main/java/lk/ijse/dao/custom/PaymentDAO.java
@@ -0,0 +1,12 @@
+package lk.ijse.dao.custom;
+
+import lk.ijse.dao.SuperDAO;
+import lk.ijse.entity.Payment;
+
+import java.util.List;
+
+public interface PaymentDAO extends SuperDAO {
+    String generateNewId() throws Exception;
+    List<Payment> getAll() throws Exception;
+    boolean savePayment(Payment payment) throws Exception;
+}
